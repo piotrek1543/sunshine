63f578af57e9ac64c37266427bbf6a3a37f5f6b0
Piotr Ekert <piotrekert90@gmail.com>
2022-11-27 15:04:22 +0100

    Add required dependencies.

---
+++ app/src/main/java/com/example/sunshine/SunshineApplication.kt
@@ -0,0 +1,15 @@
+package com.example.sunshine
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+
+@HiltAndroidApp
+class SunshineApplication : Application() {
+
+    override fun onCreate() {
+        Timber.plant(Timber.DebugTree())
+        super.onCreate()
+    }
+}
